b62bb2d1d8f942c9b6932c6e39fe8cda57b53090
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-05-13 11:32:05 +0300

    feat : add p4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,54 @@
+const fs = require('fs');
+const path = require('path');
 
+
+function copyDir(){
+  let adress = path.resolve(__dirname, 'files-copy'); 
+
+  fs.access(adress, fs.constants.F_OK, (err) => {
+    if (err) {
+      copyRecursive();
+    } else {
+      fs.readdir(adress, {withFileTypes: true}, (err, files) => {
+        files.forEach(function(result) {  
+          if(result.isFile()){
+            let name = result.name;         
+            let dests = path.join(__dirname, 'files-copy', name);   
+            fs.unlink(dests, (err) => {
+              if (err) throw err;           
+            });
+          }});
+      });
+      setTimeout(()=> {fs.rmdir(adress, (err) => {
+        if (err) throw 'not Удалили папку';             
+      });}, 100 );
+      setTimeout(()=> {copyRecursive();}, 300 ); 
+    }});       
+} 
+
+copyDir(); 
+
+let src = path.join(__dirname, 'files');
+let dest = path.join(__dirname, 'files-copy');
+
+function copyRecursive() {
+  
+  fs.mkdir(dest, { recursive: true }, (err) => {
+    if (err) throw err;  
+    console.log('Создана новая папка files-copy');
+  });
+  fs.readdir(src, {withFileTypes: true}, (err, files) => {
+    if (err) throw err;    
+    files.forEach(function(result) {  
+      if(result.isFile()){
+        let name = result.name;
+        let srcs = path.join(__dirname, 'files', name);
+        let dests = path.join(__dirname, 'files-copy', name);            
+        fs.copyFile(srcs, dests, (err) => {
+          if (err) throw err;  
+          console.log('Файл успешно копирован');
+        });
+      }});  
+  });   
+  
+}
